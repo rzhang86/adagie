a022f7aca8ef6fd45552371c909bcf65bb65eb61
Ray <rzhang86@gmail.com>
2013-06-27 00:54:41 -0400

    changed name to adagie

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "adder"
+  val appName         = "adagie"
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
